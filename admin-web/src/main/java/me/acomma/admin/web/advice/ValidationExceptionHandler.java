b11062233976adcf83536009c58d6987494602f0
acomma <acomma@yeah.net>
2022-11-20 18:18:35 +0800

    feature: 参数校验异常拦截器

---
+++ admin-web/src/main/java/me/acomma/admin/web/advice/ValidationExceptionHandler.java
@@ -0,0 +1,31 @@
+package me.acomma.admin.web.advice;
+
+import me.acomma.admin.common.Result;
+import me.acomma.admin.common.enums.SystemErrorCode;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE - 4)
+public class ValidationExceptionHandler {
+    @ExceptionHandler
+    public Result<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ObjectError error = exception.getAllErrors().get(0);
+        String message = error.getDefaultMessage();
+        return Result.<Void>builder().code(SystemErrorCode.INVALID_PARAMETER.code()).message(message).build();
+    }
+
+    @ExceptionHandler
+    public Result<Void> handleConstraintViolationException(ConstraintViolationException exception) {
+        ConstraintViolation<?> violation = exception.getConstraintViolations().iterator().next();
+        String message = violation.getMessage();
+        return Result.<Void>builder().code(SystemErrorCode.INVALID_PARAMETER.code()).message(message).build();
+    }
+}
